bd76016b79f0e13ce47a73e6edd0a36a24fa0f60
veaba <908662421@qq.com>
2019-10-14 18:01:24 +0800

    update scripts

--- deploy/build.sh
+++ deploy/build.sh
@@ -28,7 +28,8 @@ set -e
 
 # git push --set-upstream "https://veaba:${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git" master:gh-pages
 
-git push -f "https://veaba:${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git" master:gh-pages
+
+echo "git push -f https://veaba:${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git master:gh-pages"
 # git push -f "https://veaba:${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git" master:gh-pages
 
 echo "漂亮！部署成功： `date`"
