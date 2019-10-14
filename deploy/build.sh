02739745fa65da65becfd04863e9ef23fd9689f2
veaba <908662421@qq.com>
2019-10-14 15:59:33 +0800

    update 2019年10月14日15:59:32

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,25 +1,19 @@
-# pwd 
-# ls
-# cd docs/.vuepress/dist
-# ls
-# git init
-# git add .
+cd docs/.vuepress/dist
+git init
+git add .
 # git checkout -b gh-pages
 
-# #  把dist 发布到新分支
+#  把dist 发布到新分支
 
 # # 配置git
 # git config user.name veaba
 # git config user.email 908662421@qq.com
 
-# # 抛出错误
-# set -e 
-# git add .
-# git commit -m "[Deploy sucess]：`date`"
+# 抛出错误
+set -e 
+git commit -m "[Deploy sucess]：`date`"
 # git remote add origin git@github.com:veaba/vuepress.git
 # git push -f git@github.com:veaba/vuepress.git master:gh-pages
-# git push -f "https://oauth2:${secrets.ACTIONS_ACCESS_TOKEN_VUEPRESS__CI}@github.com:veaba/vuepress.git master:gh-pages"
-echo ${ACCESS_TOKEN}
-echo ${ACCESS_TOKEN_1}
-export
+git push -f "${ACCESS_TOKEN_PUSH}@github.com:veaba/vuepress.git master:gh-pages"
+
 echo "漂亮！部署成功： `date`"
