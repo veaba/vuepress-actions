ddb25256d412fff67a1fb23c16d2e852febc95b5
veaba <908662421@qq.com>
2019-10-14 19:08:23 +0800

    update scripts

--- deploy/build.sh
+++ deploy/build.sh
@@ -2,8 +2,8 @@ cd docs/
 git init
 
 # 配置git
-git config --global user.name "veaba"
-git config --global user.email "908662421@qq.com"
+git config user.name "veaba"
+git config user.email "908662421@qq.com"
 
 # git提交
 git add .
@@ -13,7 +13,9 @@ git commit -m "[Deploy sucess]：`date`"
 set -e 
 
 # 用echo 拼装，否则会无效！！测试了上百次的结果
-echo "`git push -f https://${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git master:gh-pages`"
+# echo "`git push -f https://veaba:${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git master:gh-pages`"
 # git push -f "https://${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git" master:gh-pages
+
+echo "https://veaba:${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git master:gh-pages"
 
 echo "漂亮！部署成功： `date`"
