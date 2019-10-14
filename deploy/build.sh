617d5538038d326a7ecb84be001d5b424906a485
veaba <908662421@qq.com>
2019-10-14 16:12:43 +0800

    update master

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,18 +1,17 @@
 cd docs/.vuepress/dist
 git init
 git add .
-# git checkout -b gh-pages
-
-#  把dist 发布到新分支
 
 # 配置git
-git config user.name veaba
+git config --gloabl user.name "veaba"
+git config --gloabel user.email "908662421@qq.com"
 
 # 抛出错误
 set -e 
 git commit -m "[Deploy sucess]：`date`"
 # git remote add origin git@github.com:veaba/vuepress.git
 # git push -f git@github.com:veaba/vuepress.git master:gh-pages
-git push -f "https://${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git master:gh-pages"
+git push -f --set-upstream "https://${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git master:gh-pages"
+# git push -f "https://${ACCESS_TOKEN_PUSH}@github.com/veaba/vuepress.git master:gh-pages"
 
 echo "漂亮！部署成功： `date`"
