ac037b92126a1046521bfd22f7268756762211fb
Jigaozheng <godpu@outlook.com>
2020-01-19 16:55:14 +0800

    update script setting cname

--- deploy/build.sh
+++ deploy/build.sh
@@ -23,7 +23,10 @@ cd "${PUBLISH_DIR}" # ./docs/.vuepress/dist
 
 # 为gh-pages 生成CNAME，发现使用别人提供的脚本，生成的竟然是小写的CNAME文件，所以改为小写的，使用脚本写入
 
-echo "vuepress.datav.ai" >CNAME
+# 设置CNAME
+if [ -z "${CNAME}" ]; then 
+    echo "${CNAME}">CNAME
+fi 
 
 # 格式化的输出
 function print_error() {
